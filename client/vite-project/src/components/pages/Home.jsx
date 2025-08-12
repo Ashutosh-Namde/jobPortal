7a6866e807c23369a8e3ac13b9a01766a9059993
Ashutosh Namdev <namdevashutosh38@gmail.com>
2025-08-12 15:24:41 +0530

    qwertyuiop

--- client/vite-project/src/components/pages/Home.jsx
+++ client/vite-project/src/components/pages/Home.jsx
@@ -8,12 +8,12 @@ const Home = () => {
   
   return (
     <div>
-         {/* <Nav/> */}
+         <Nav/>
          <HeroSection/>
          <CategoryCarousel/>
          {/* <LatestJobs/> */}
          {/* <Footer/> */}
-        Home2 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo neque eaque suscipit recusandae saepe esse placeat, illo optio eum dignissimos, modi numquam ab ad rerum repudiandae ipsam ullam debitis corporis? Cumque rerum quam, molestiae illum magni, officia sed quidem veniam ab explicabo tempore, a unde! Magni possimus saepe repellendus provident iure perferendis cupiditate porro, ut assumenda a, unde cum alias maiores ab recusandae perspiciatis animi ratione laboriosam ex, nisi architecto. Aliquid vero aut neque autem esse delectus beatae ut similique voluptate, qui temporibus libero voluptatem nam nisi vel? Autem ad eaque tenetur, animi eveniet consequuntur consectetur fugit saepe rem reprehenderit?</div>
+        Home21234567890 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo neque eaque suscipit recusandae saepe esse placeat, illo optio eum dignissimos, modi numquam ab ad rerum repudiandae ipsam ullam debitis corporis? Cumque rerum quam, molestiae illum magni, officia sed quidem veniam ab explicabo tempore, a unde! Magni possimus saepe repellendus provident iure perferendis cupiditate porro, ut assumenda a, unde cum alias maiores ab recusandae perspiciatis animi ratione laboriosam ex, nisi architecto. Aliquid vero aut neque autem esse delectus beatae ut similique voluptate, qui temporibus libero voluptatem nam nisi vel? Autem ad eaque tenetur, animi eveniet consequuntur consectetur fugit saepe rem reprehenderit?</div>
   )
 }
 
--- client/vite-project/src/components/share/CategoryCarousel.jsx
+++ client/vite-project/src/components/share/CategoryCarousel.jsx
@@ -1,32 +1,39 @@
 import React from 'react'
-import {Carousel , CarouselContent ,CarouselItem , CarouselPrevious} from '../ui/carousel'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselPrevious,
+  CarouselNext
+} from '../ui/carousel'
 import { Button } from '../ui/button'
 
-
 const CategoryCarousel = () => {
-    const category = [
-        "Frontend Developer",
-        "Backend Developer",
-        "Data Science",
-        "Graphic Designer",
-        "Full Stack Developer"
-    ]
+  const category = [
+    // "Frontend veloper",
+    "Backend Developer",
+    "Data Science",
+    "Graphic Designer",
+    "Full Stack Developer"
+  ]
+
   return (
     <div className='w-full max-w-xl mx-auto my-20'>
-        <Carousel>
-            < CarouselContent>
-                {
-                   category.map((cat,index)=>(
-                      <CarouselItem className={"md:basic-1/2 lg-basic-1/3"}>
-                        <Button className={"rounded-full"}>{cat}</Button>
-                      </CarouselItem>
-                   ))
-                }
-            </CarouselContent>
-            <CarouselPrevious/>
-        </Carousel>
+      <Carousel className="w-full">
+        <CarouselContent>
+          {category.map((cat, index) => (
+            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3 flex justify-center">
+              <Button className="rounded-full">{cat}</Button>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+
+        {/* Navigation Buttons */}
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
-  )
+  )   
 }
 
 export default CategoryCarousel
