a8794e1f6872e50aadf2d52d742aeddda7e1d3ee
Ashutosh Namdev <namdevashutosh38@gmail.com>
2025-08-12 15:14:15 +0530

    new change

--- client/vite-project/src/components/pages/Home.jsx
+++ client/vite-project/src/components/pages/Home.jsx
@@ -4,14 +4,16 @@ import HeroSection from '../share/HeroSection'
 import CategoryCarousel from '../share/CategoryCarousel'
 
 const Home = () => {
+  console.log("home page");
+  
   return (
     <div>
-         <Nav/>
+         {/* <Nav/> */}
          <HeroSection/>
          <CategoryCarousel/>
          {/* <LatestJobs/> */}
          {/* <Footer/> */}
-        Home</div>
+        Home2 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo neque eaque suscipit recusandae saepe esse placeat, illo optio eum dignissimos, modi numquam ab ad rerum repudiandae ipsam ullam debitis corporis? Cumque rerum quam, molestiae illum magni, officia sed quidem veniam ab explicabo tempore, a unde! Magni possimus saepe repellendus provident iure perferendis cupiditate porro, ut assumenda a, unde cum alias maiores ab recusandae perspiciatis animi ratione laboriosam ex, nisi architecto. Aliquid vero aut neque autem esse delectus beatae ut similique voluptate, qui temporibus libero voluptatem nam nisi vel? Autem ad eaque tenetur, animi eveniet consequuntur consectetur fugit saepe rem reprehenderit?</div>
   )
 }
 
