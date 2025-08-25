4a72779af54dfa975486f13de690071090b60c10
Ashutosh Namdev <namdevashutosh38@gmail.com>
2025-08-25 22:44:16 +0530

    url changed

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 const corsOption = {
-    origin: "http://localhost:5173", 
+    origin: "job-portal-ashen-seven.vercel.app", 
     credentials: true                
 };
 
