90357223b04332ff39e635bd0d0cfbb2e5471eda
Ashutosh Namdev <namdevashutosh38@gmail.com>
2025-08-29 00:43:23 +0530

    /route

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,13 @@ config();
 connectDB()
 
 
+app.get('/',(req,res)=>{
+    res.send({
+        activeStatus:true,
+        error:false,
+    })
+})
+
 
 app.listen(process.env.PORT,()=>{
     console.log(`server is runing on port ${process.env.PORT}`);
