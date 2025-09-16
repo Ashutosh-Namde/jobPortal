83be42ef2856376bddd21b0ae328a0738fd87dd4
Ashutosh Namdev <namdevashutosh38@gmail.com>
2025-09-16 23:46:50 +0530

    for deploy

--- client/vite-project/src/components/utils/context.jsx
+++ client/vite-project/src/components/utils/context.jsx
@@ -1,3 +1,3 @@
-// export const API = "https://job-portal-h5dx.vercel.app"
-export const API = "http://localhost:3000"
+export const API = "https://job-portal-h5dx.vercel.app"
+// export const API = "http://localhost:3000"
 
